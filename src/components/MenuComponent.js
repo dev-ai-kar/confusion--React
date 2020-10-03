19187145fc4b6524bb9ce84968a65a0b567d374b
Vaibhav Deokar <deokar.vaibhav@outlook.com>
2020-10-03 10:21:59 +0530

    Presentational and Container Components
    *Organize React app into presentational and container components
    *Enable presentational components to be concerned
    with the look and feel of app.
    *Enable container components to deal with the state,
    provide the data and handle user interactions.

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,43 +1,11 @@
 import React, { Component } from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
-import DishDetail from './DishdetailComponent'
 
 class Menu extends Component {
 
   constructor(props) {
       super(props);
-
-      this.state = {
-          selectedDish: null
-      }
-      console.log('Menu Component Constructor is invoked')
   }
-
-  componentDidMount() {
-    console.log('Menu Component compoenent did mount is invoked')
-  }
-
-  onDishSelect(dish) {
-      this.setState({ selectedDish: dish});
-  }
-
-  renderDish(dish) {	
-    if (dish != null) {	
-        return  (	
-            <Card>	
-                <CardImg width="100%" src={dish.image} alt={dish.name}></CardImg>	
-                <CardBody>	
-                    <CardTitle>{dish.name}</CardTitle>	
-                    <CardText>{dish.description}</CardText>	
-                </CardBody>	
-            </Card>	
-        );	
-    } else {	
-        return (	
-            <div></div>	
-        );	
-    }	
-}	
 
 
   render() {
@@ -45,7 +13,7 @@ class Menu extends Component {
           return (
             <div  className="col-12 col-md-5 m-1">
               <Card key={dish.id}
-                onClick={() => this.onDishSelect(dish)}>
+                        onClick={() => this.props.onClick(dish.id)}>
                 <CardImg width="100%" src={dish.image} alt={dish.name} />
                 <CardImgOverlay>
                     <CardTitle>{dish.name}</CardTitle>
@@ -62,7 +30,6 @@ class Menu extends Component {
         <div className="row">
           {menu}
         </div>
-        <DishDetail dish={this.state.selectedDish}></DishDetail>
       </div>
     );
   }
